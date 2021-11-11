143311e4e4144f840c49e3f4696f957c168df9e8
Ivan Kosolapov <polzuka@gmail.com>
2021-11-11 12:46:33 +0500

    Added status types

--- dist/declares/main.d.ts
+++ dist/declares/main.d.ts
@@ -1,4 +1,13 @@
 export {Entities} from "./entities"
 export {ApiClient} from "./ApiClient"
-export {getProductName, getOptionName, getUnitName} from "./functions"
-export {AVAILABILITY_STATUSES, BOOKING_STATUSES} from "./consts"
+export {
+    getProductName,
+    getOptionName,
+    getUnitName,
+} from "./functions"
+export {
+    AVAILABILITY_STATUSES,
+    BOOKING_STATUSES,
+    AVAILABILITY_STATUSES_UNION,
+    BOOKING_STATUSES_UNION,
+} from "./consts"
