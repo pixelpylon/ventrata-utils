e3968708cef0908dbed087b0b5da190ccebc52f2
Ivan Kosolapov <polzuka@gmail.com>
2021-11-29 11:16:21 +0500

    Added getVisitorCountersWithoutInfants()

---
+++ dist/getVisitorCountersWithoutInfants.js
@@ -0,0 +1,10 @@
+const {VISITOR_AGES} = require("./consts");
+const {cloneDeep} = require("lodash");
+
+const getVisitorCountersWithoutInfants = (counters) => {
+  const clonedCounters = cloneDeep(counters);
+  clonedCounters[VISITOR_AGES.INFANTS] = 0;
+  return clonedCounters;
+};
+
+module.exports = getVisitorCountersWithoutInfants;
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 const getPhone = require('./getPhone');
 const countVisitors = require('./countVisitors');
 const getEmptyVisitorCounters = require('./getEmptyVisitorCounters');
+const getVisitorCountersWithoutInfants = require('./getVisitorCountersWithoutInfants');
 const formatVisitorCounters = require('./formatVisitorCounters');
 const getProductName = require('./getProductName');
 const getOptionName = require('./getOptionName');
@@ -17,6 +18,7 @@ module.exports = {
   getPhone,
   countVisitors,
   getEmptyVisitorCounters,
+  getVisitorCountersWithoutInfants,
   formatVisitorCounters,
   ApiClient,
   ApiError,
