fe09d888436121364d05f608ee5a50213a2a87e7
Ivan Kosolapov <polzuka@gmail.com>
2021-12-14 13:59:57 +0500

    Fixed ApiClient

--- dist/ApiClient.js
+++ dist/ApiClient.js
@@ -7,151 +7,160 @@ const {AxiosApiClient} = require("common-utils")
 const DATE_FORMAT = 'YYYY-MM-DD'
 
 class ApiClient {
-  constructor (apiKey) {
-    const axiosInstance = axios.create({
-      baseURL: 'https://api.ventrata.com/octo/',
-      headers: {Authorization: `Bearer ${apiKey}`}
-    })
+    constructor(apiKey) {
+        const axiosInstance = axios.create({
+            baseURL: 'https://api.ventrata.com/octo/',
+            headers: {Authorization: `Bearer ${apiKey}`}
+        })
 
-    axiosRetry(axiosInstance, {
-      retries: 3,
-      retryDelay: axiosRetry.exponentialDelay,
-    })
+        axiosRetry(axiosInstance, {
+            retries: 3,
+            retryDelay: axiosRetry.exponentialDelay,
+        })
 
-    this.axiosApiClient = AxiosApiClient(axiosInstance)
-  }
+        this.axiosApiClient = AxiosApiClient(axiosInstance)
+    }
 
-  getProduct (productId) {
-    return this.axiosApiClient.get(`products/${productId}`)
-  }
+    getProduct(productId) {
+        return this.axiosApiClient.get(`products/${productId}`)
+            .then(({data}) => data)
+    }
 
-  getBooking (bookingUuid) {
-    return this.axiosApiClient.get(`bookings/${bookingUuid}`)
-  }
+    getBooking(bookingUuid) {
+        return this.axiosApiClient.get(`bookings/${bookingUuid}`)
+            .then(({data}) => data)
+    }
 
-  getBookings ({
-      resellerReference,
-      supplierReference,
-      localDate,
-      localDateStart,
-      localDateEnd,
-      productId,
-      optionId,
-      utcCreatedAtStart,
-      utcCreatedAtEnd,
-  }) {
-    return this.axiosApiClient.get(`bookings`, {
-      params: {
-        resellerReference,
-        supplierReference,
-        localDate,
-        localDateStart,
-        localDateEnd,
-        productId,
-        optionId,
-        utcCreatedAtStart,
-        utcCreatedAtEnd,
-      },
-    })
-  }
+    getBookings({
+                    resellerReference,
+                    supplierReference,
+                    localDate,
+                    localDateStart,
+                    localDateEnd,
+                    productId,
+                    optionId,
+                    utcCreatedAtStart,
+                    utcCreatedAtEnd,
+                }) {
+        return this.axiosApiClient.get(`bookings`, {
+            params: {
+                resellerReference,
+                supplierReference,
+                localDate,
+                localDateStart,
+                localDateEnd,
+                productId,
+                optionId,
+                utcCreatedAtStart,
+                utcCreatedAtEnd,
+            },
+        })
+            .then(({data}) => data)
+    }
 
-  getCalendar ({productId, optionId, units, localDateStart, localDateEnd}) {
-    return this.axiosApiClient.post(`availability/calendar`, {
-      productId,
-      optionId,
-      localDateStart,
-      localDateEnd,
-      units,
-    })
-  }
+    getCalendar({productId, optionId, units, localDateStart, localDateEnd}) {
+        return this.axiosApiClient.post(`availability/calendar`, {
+            productId,
+            optionId,
+            localDateStart,
+            localDateEnd,
+            units,
+        })
+            .then(({data}) => data)
+    }
 
-  getAvailabilities ({productId, optionId, units, localDateStart, localDateEnd}) {
-    return this.axiosApiClient.post('post',`availability`, {
-      productId,
-      optionId,
-      localDateStart,
-      localDateEnd,
-      units,
-    })
-  }
+    getAvailabilities({productId, optionId, units, localDateStart, localDateEnd}) {
+        return this.axiosApiClient.post('post', `availability`, {
+            productId,
+            optionId,
+            localDateStart,
+            localDateEnd,
+            units,
+        })
+            .then(({data}) => data)
+    }
 
-  getMonthCalendar ({productId, optionId, units, year, month}) {
-    const startDate = moment({year, month, date: 1})
-    const endDate = moment({year, month, date: startDate.daysInMonth()})
+    getMonthCalendar({productId, optionId, units, year, month}) {
+        const startDate = moment({year, month, date: 1})
+        const endDate = moment({year, month, date: startDate.daysInMonth()})
 
-    return this.getCalendar({
-      productId,
-      optionId,
-      localDateStart: startDate.format(DATE_FORMAT),
-      localDateEnd: endDate.format(DATE_FORMAT),
-      units,
-    })
-  }
+        return this.getCalendar({
+            productId,
+            optionId,
+            localDateStart: startDate.format(DATE_FORMAT),
+            localDateEnd: endDate.format(DATE_FORMAT),
+            units,
+        })
+    }
 
-  getDateAvailabilities ({productId, optionId, units, month, year, date}) {
-    const localDate = moment({year, month, date}).format(DATE_FORMAT)
+    getDateAvailabilities({productId, optionId, units, month, year, date}) {
+        const localDate = moment({year, month, date}).format(DATE_FORMAT)
 
-    return this.getAvailabilities({
-      productId,
-      optionId,
-      localDateStart: localDate,
-      localDateEnd: localDate,
-      units,
-    })
-  }
+        return this.getAvailabilities({
+            productId,
+            optionId,
+            localDateStart: localDate,
+            localDateEnd: localDate,
+            units,
+        })
+    }
 
-  createBooking ({bookingUuid, productId, optionId, availabilityId, units, notes}) {
-    return this.axiosApiClient.post(`/bookings`,
-      {
-        uuid: bookingUuid,
-        productId,
-        optionId,
-        availabilityId,
-        notes,
-        unitItems: units.reduce((result , unitCounter) => {
-          const unitItems = times(unitCounter.quantity, () => ({unitId: unitCounter.id}))
-          result.push(...unitItems)
-          return result
-        }, [])
-      }
-    )
-  }
+    createBooking({bookingUuid, productId, optionId, availabilityId, units, notes}) {
+        return this.axiosApiClient.post(`/bookings`,
+            {
+                uuid: bookingUuid,
+                productId,
+                optionId,
+                availabilityId,
+                notes,
+                unitItems: units.reduce((result, unitCounter) => {
+                    const unitItems = times(unitCounter.quantity, () => ({unitId: unitCounter.id}))
+                    result.push(...unitItems)
+                    return result
+                }, [])
+            }
+        )
+            .then(({data}) => data)
+    }
 
-  confirmBooking ({bookingUuid, emailAddress, fullName, phoneNumber, locales, country, resellerReference}) {
-    return this.axiosApiClient.post(`/bookings/${bookingUuid}/confirm`,
-      {
-        contact: {
-          fullName,
-          emailAddress,
-          phoneNumber,
-          locales,
-          country,
-        },
-        resellerReference,
-      }
-    )
-  }
+    confirmBooking({bookingUuid, emailAddress, fullName, phoneNumber, locales, country, resellerReference}) {
+        return this.axiosApiClient.post(`/bookings/${bookingUuid}/confirm`,
+            {
+                contact: {
+                    fullName,
+                    emailAddress,
+                    phoneNumber,
+                    locales,
+                    country,
+                },
+                resellerReference,
+            }
+        )
+            .then(({data}) => data)
+    }
 
-  updateBooking ({bookingUuid, productId, optionId, availabilityId, units, notes}) {
-    return this.axiosApiClient.patch(`/bookings/${bookingUuid}`,
-      {
-        uuid: bookingUuid,
-        productId,
-        optionId,
-        availabilityId,
-        notes,
-        unitItems: units.reduce((result , unitCounter) => {
-          const unitItems = times(unitCounter.quantity, () => ({unitId: unitCounter.id}))
-          result.push(...unitItems)
-          return result
-        }, [])
-      }
-    )
-  }
+    updateBooking({bookingUuid, productId, optionId, availabilityId, units, notes}) {
+        return this.axiosApiClient.patch(`/bookings/${bookingUuid}`,
+            {
+                uuid: bookingUuid,
+                productId,
+                optionId,
+                availabilityId,
+                notes,
+                unitItems: units.reduce((result, unitCounter) => {
+                    const unitItems = times(unitCounter.quantity, () => ({unitId: unitCounter.id}))
+                    result.push(...unitItems)
+                    return result
+                }, [])
+            }
+        )
+            .then(({data}) => data)
+    }
 
-  cancelBooking ({bookingUuid}) {
-    return this.axiosApiClient.delete(`/bookings/${bookingUuid}`)
-  }
+    cancelBooking({bookingUuid}) {
+        return this.axiosApiClient.delete(`/bookings/${bookingUuid}`)
+            .then(({data}) => data)
+    }
 }
 
 module.exports = ApiClient
