b408f8fcf129f522c47f5ae4bd3f426b2d10f0fd
Ivan Kosolapov <polzuka@gmail.com>
2022-05-04 09:21:07 +0500

    Declaring of visitor counter functions

--- dist/declares/functions.d.ts
+++ dist/declares/functions.d.ts
@@ -1,8 +1,12 @@
 import {Entities} from "./entities";
-import {UNIT_TYPES_UNION} from "./consts";
+import {UNIT_TYPES_UNION, VISITOR_AGES_UNION} from "./consts";
 
 export function getProductName (product: Entities.IProduct): string
 export function getOptionName (option: Entities.IOption): string
 export function getUnitName (unit: Entities.IUnit): string
 export function getUnitMapping (product: Entities.IProduct): {[key in UNIT_TYPES_UNION]: string}
 export function getUnitMapper (product: Entities.IProduct): (type: UNIT_TYPES_UNION) => string
+export function getEmptyVisitorCounters (): {[key in VISITOR_AGES_UNION]: number}
+export function countVisitors (booking: Entities.IBooking): {[key in VISITOR_AGES_UNION]: number}
+export function getVisitorCountersWithoutInfants (booking: Entities.IBooking): {[key in VISITOR_AGES_UNION]: number}
+export function formatVisitorCounters (counters: {[key in VISITOR_AGES_UNION]: number}): string
--- dist/declares/main.d.ts
+++ dist/declares/main.d.ts
@@ -6,13 +6,19 @@ export {
     getUnitName,
     getUnitMapping,
     getUnitMapper,
+    formatVisitorCounters,
+    getEmptyVisitorCounters,
+    getVisitorCountersWithoutInfants,
+    countVisitors,
 } from "./functions"
 export {
     AVAILABILITY_STATUSES,
     BOOKING_STATUSES,
     UNIT_TYPES,
+    VISITOR_AGES,
     AVAILABILITY_STATUSES_UNION,
     BOOKING_STATUSES_UNION,
     UNIT_TYPES_UNION,
+    VISITOR_AGES_UNION,
     DEFAULT_OPTION,
 } from "./consts"
