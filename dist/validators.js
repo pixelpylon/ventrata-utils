5e5375dafc856d50d8e2537e36fc80ea535b7b05
Ivan Kosolapov <polzuka@gmail.com>
2022-10-14 11:29:45 +0500

    Fixed unit id validation

--- dist/validators.js
+++ dist/validators.js
@@ -1,19 +1,21 @@
 const {validate} = require("common-utils")
 const {UNIT_TYPES} = require("./consts")
 
+const UNIT_ID_FORMAT = /^unit_[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
+
 const typeUnitCountersValidator = validate.items(validate.props({
     type: validate.isIn([UNIT_TYPES.ADULT, UNIT_TYPES.CHILD, UNIT_TYPES.CHILD]),
     quantity: validate.isInt,
 }))
 
 const idUnitCountersValidator = validate.items(validate.props({
-    id: validate.isUuid,
+    id: validate.isFormat(UNIT_ID_FORMAT),
     quantity: validate.isInt,
 }))
 
 const combinedUnitCountersValidator = validate.items(validate.props({
     type: validate.isIn([UNIT_TYPES.ADULT, UNIT_TYPES.CHILD, UNIT_TYPES.CHILD]),
-    id: validate.isUuid,
+    id: validate.isFormat(UNIT_ID_FORMAT),
     quantity: validate.isInt,
 }))
 
